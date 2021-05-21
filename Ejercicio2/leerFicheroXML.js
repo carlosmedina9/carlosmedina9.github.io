927fd2a39c16cac62ad70e530fcc397bc686dd31
carlosmedina9 <77376979+carlosmedina9@users.noreply.github.com>
2021-05-21 13:31:53 +0200

    Add files via upload

---
+++ Ejercicio2/leerFicheroXML.js
@@ -0,0 +1,12 @@
+function gestionarFicheroXML(xmlDoc){
+	let capaVacia = document.querySelector("#ficheroXML")
+	let derecha = xmlDoc.querySelectorAll("emisor")
+	let izquierda = xmlDoc.querySelectorAll("receptor")
+	let fondo = xmlDoc.querySelectorAll("conversacion")
+
+	for(let i=0; i<derecha.length&&izquierda.length; i++)
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='emisor'>" + derecha[i].textContent + "</p>"
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='receptor'>" + izquierda[i].textContent + "</p>"
+	
+
+	loadDocA("libros.xml","xml");
