5fbe4e39bc73ca1939b195d1089c625e48d55288
carlosmedina9 <77376979+carlosmedina9@users.noreply.github.com>
2021-05-21 12:54:19 +0200

    Update leerFicheroXML.js

--- Ejercicio1/leerFicheroXML.js
+++ Ejercicio1/leerFicheroXML.js
@@ -3,7 +3,7 @@ function gestionarFicheroXML(xmlDoc){
 	let capaVacia = document.querySelector("#ficheroXML")
 	let libros = xmlDoc.querySelectorAll("libro")
 	for(let i=0; i<libros.length; i++)
-	if(i==2){
+	if(i==2)
 	{
 		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='color'>" + libros[i].textContent + "</p>"
 	} else{
